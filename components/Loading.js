63feb5832a5765b4e411c3bf460c2d4ebe15bd0d
aliaaabobakr12 <aliaaabobakr12@gmail.com>
2024-02-13 22:22:09 +0200

    Update Loading component's className to include min-h-screen

--- components/Loading.js
+++ components/Loading.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export default function Loading() {
   return (
     <motion.div
-      className="flex h-screen items-center justify-center absolute bg-white z-40 w-full"
+      className="flex min-h-screen items-center justify-center absolute bg-white z-40 w-full"
       initial={{ x: 0 }}
       animate={{ x: -2000, opacity: 30 }}
       transition={{
