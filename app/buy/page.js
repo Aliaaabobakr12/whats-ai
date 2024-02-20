2116c92e99467b9b5f189be9261d1196045ad082
levwtech <abdelraahmanmostafa@gmail.com>
2024-02-20 16:59:45 +0200

    feat: add iframe for gumroad product page in /buy

---
+++ app/buy/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Buy() {
+  return (
+    <div style={{ position: "absolute", left: 0, right: 0, bottom: 0, top: 0, overflow: "hidden"}}>
+        <iframe
+            width="100%"
+            height="100%"
+            frameborder="0"
+            src="https://levw.gumroad.com/l/whatsapp"
+        ></iframe>
+    </div>
+  );
+}
